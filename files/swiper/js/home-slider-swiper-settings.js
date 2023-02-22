4b55cf32c075155273f9be6ae1655e978888b83a
Роман <69018645+Hetman-Novel@users.noreply.github.com>
2023-02-22 21:12:35 +0300

    Add files via upload

---
+++ files/swiper/js/home-slider-swiper-settings.js
@@ -0,0 +1,247 @@
+new Swiper('#one-slider', {
+   navigation: {
+      prevEl: '#one-swiper-button-prev',
+      nextEl: '#one-swiper-button-next',
+   },
+   simulateTouch: false,
+   slideToClickedSlide: true,
+   slidesPerView: 5,
+   slidesPerGroup: 1,
+   watchOverflow: true,
+   spaceBetween: 10,
+   speed: 800,
+   effect: 'slide',
+   clickable: false,
+   breakpoints: {
+      100: {
+         slidesPerView: 4,
+         slidesPerGroup: 1,
+         spaceBetween: 9,
+         direction: 'vertical',
+         clickable: false,
+      },
+      577: {
+         slidesPerView: 2,
+         slidesPerGroup: 1,
+         spaceBetween: 10,
+         direction: 'horizontal',
+         autoHeight: true,
+         clickable: false,
+      },
+      721: {
+         slidesPerView: 3,
+         slidesPerGroup: 1,
+         spaceBetween: 10,
+         clickable: false,
+      },
+      1281: {
+         slidesPerView: 5,
+         slidesPerGroup: 1,
+         spaceBetween: 10,
+         clickable: false,
+      }
+   },
+   // Отключить предзагрузка картинок
+   preloadImages: false,
+
+   // Lazy Loading (подгрузка картинок)
+   // Отключить предзагрузка картинок
+   preloadImages: false,
+
+   // Lazy Loading (подгрузка картинок)
+   lazy: {
+      // Подгружать на старте переключения слайда
+      loadOnTransitionStart: false,
+
+      // Подгрузить предыдущую и следующую картинку
+      loadPrewNext: false,
+   },
+   // Слежка за видимыми слайдами
+   watchSlidesProgress: true,
+
+   // Добавление класса видимым слайдам
+   watchSlidesVisibility: true,
+});
+
+new Swiper('#two-slider', {
+   navigation: {
+      prevEl: '#two-button-prev',
+      nextEl: '#two-button-next',
+   },
+   simulateTouch: false,
+   slideToClickedSlide: true,
+   slidesPerView: 5,
+   slidesPerGroup: 1,
+   watchOverflow: true,
+   spaceBetween: 10,
+   speed: 800,
+   effect: 'slide',
+   clickable: false,
+   breakpoints: {
+      200: {
+         slidesPerView: 4,
+         spaceBetween: 9,
+         direction: 'vertical',
+         clickable: false,
+      },
+      577: {
+         slidesPerView: 2,
+         spaceBetween: 10,
+         direction: 'horizontal',
+         autoHeight: true,
+         clickable: false,
+      },
+      721: {
+         slidesPerView: 3,
+         spaceBetween: 10,
+         clickable: false,
+      },
+      1281: {
+         slidesPerView: 5,
+         spaceBetween: 10,
+         clickable: false,
+      }
+   },
+   // Отключить предзагрузка картинок
+   preloadImages: false,
+
+   // Lazy Loading (подгрузка картинок)
+   // Отключить предзагрузка картинок
+   preloadImages: false,
+
+   // Lazy Loading (подгрузка картинок)
+   lazy: {
+      // Подгружать на старте переключения слайда
+      loadOnTransitionStart: false,
+
+      // Подгрузить предыдущую и следующую картинку
+      loadPrewNext: false,
+   },
+   // Слежка за видимыми слайдами
+   watchSlidesProgress: true,
+
+   // Добавление класса видимым слайдам
+   watchSlidesVisibility: true,
+});
+
+new Swiper('#three-slider', {
+   navigation: {
+      prevEl: '#hree-button-prev',
+      nextEl: '#hree-button-next',
+   },
+   simulateTouch: false,
+   slideToClickedSlide: true,
+   slidesPerView: 5,
+   slidesPerGroup: 1,
+   watchOverflow: true,
+   spaceBetween: 10,
+   speed: 800,
+   effect: 'slide',
+   clickable: false,
+   breakpoints: {
+      200: {
+         slidesPerView: 4,
+         spaceBetween: 9,
+         direction: 'vertical',
+         clickable: false,
+      },
+      577: {
+         slidesPerView: 2,
+         spaceBetween: 10,
+         direction: 'horizontal',
+         autoHeight: true,
+         clickable: false,
+      },
+      721: {
+         slidesPerView: 3,
+         spaceBetween: 10,
+         clickable: false,
+      },
+      1281: {
+         slidesPerView: 5,
+         spaceBetween: 10,
+         clickable: false,
+      }
+   },
+   // Отключить предзагрузка картинок
+   preloadImages: false,
+
+   // Lazy Loading (подгрузка картинок)
+   // Отключить предзагрузка картинок
+   preloadImages: false,
+
+   // Lazy Loading (подгрузка картинок)
+   lazy: {
+      // Подгружать на старте переключения слайда
+      loadOnTransitionStart: false,
+
+      // Подгрузить предыдущую и следующую картинку
+      loadPrewNext: false,
+   },
+   // Слежка за видимыми слайдами
+   watchSlidesProgress: true,
+
+   // Добавление класса видимым слайдам
+   watchSlidesVisibility: true,
+});
+
+new Swiper('#four-slider', {
+   navigation: {
+      prevEl: '#four-button-prev',
+      nextEl: '#four-button-next',
+   },
+   simulateTouch: false,
+   slideToClickedSlide: true,
+   slidesPerView: 5,
+   slidesPerGroup: 1,
+   watchOverflow: true,
+   spaceBetween: 10,
+   speed: 800,
+   effect: 'slide',
+   clickable: false,
+   breakpoints: {
+      200: {
+         slidesPerView: 4,
+         spaceBetween: 9,
+         direction: 'vertical',
+         clickable: false,
+      },
+      577: {
+         slidesPerView: 2,
+         spaceBetween: 10,
+         direction: 'horizontal',
+         autoHeight: true,
+         clickable: false,
+      },
+      721: {
+         slidesPerView: 3,
+         spaceBetween: 10,
+         clickable: false,
+      },
+      1281: {
+         slidesPerView: 5,
+         spaceBetween: 10,
+         clickable: false,
+      }
+   },
+   // Отключить предзагрузка картинок
+   preloadImages: false,
+
+   // Lazy Loading (подгрузка картинок)
+   // Отключить предзагрузка картинок
+   preloadImages: false,
+
+   // Lazy Loading (подгрузка картинок)
+   lazy: {
+      // Подгружать на старте переключения слайда
+      loadOnTransitionStart: false,
+
+      // Подгрузить предыдущую и следующую картинку
+      loadPrewNext: false,
+   },
+   // Слежка за видимыми слайдами
+   watchSlidesProgress: true,
+
+   // Добавление класса видимым слайдам
+   watchSlidesVisibility: true,
+});
